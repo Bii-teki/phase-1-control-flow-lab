1bef258eac7e5b63b79b8983a81bdf3e93c09bf6
B33 <geoffrey.bii@student.moringaschool.com>
2023-06-13 18:14:03 +0300

    assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+let ride=199;
+
+
+
+function scuberGreetingForFeet(ride){
+if (ride<=400) {
+  return 'This one is on me!';
+  
+} else if(ride>2000 && ride<2500) {
+  return 'I will gladly take your thirty bucks.';
+}
+   else if(ride>2500) {
+  return 'No can do.';
+}
+}
+const city = 'NYC';
+
+
+function ternaryCheckCity(city) {
   // Write your code here!
+return (city === 'NYC') ? `Ok, sounds good.` : `No go.`;
 }
 
-function ternaryCheckCity(){
+const tip="generous";
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      
+      case 'not as generous':
+        return 'Thank you.';
+          
+    default:
+      return 'Bye.';
+  }
 }
